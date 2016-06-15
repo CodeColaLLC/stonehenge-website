c9a64dfef7aff4ec803fe0ce42c04d60e3f4b9d5
Matt Miller <me@mmiller.me>
2016-06-14 19:47:39 -0600

    Initial setup of website using Grayscale Bootstrap theme

---
+++ webpack.loaders.js
@@ -0,0 +1,47 @@
+module.exports = [
+	{
+		test: /\.js$/,
+		exclude: /(node_modules|bower_components)/,
+		loaders: ['babel'],
+	},
+	{
+		test: /\.css$/,
+		loader: 'style-loader!css-loader'
+	},
+	{
+		test: /redactor.*\.js/,
+		loader: 'imports?jQuery=jquery,$=jquery,this=>window,CodeMirror=codemirror'
+	},
+	{
+		test: /(bootstrap|jquery\.easing).*\.js/,
+		loader: 'imports?jQuery=jquery,$=jquery,this=>window'
+	},
+	{
+		test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+		loader: 'file'
+	},
+	{
+		test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+		loader: 'url?prefix=font/&limit=5000'
+	},
+	{
+		test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+		loader: 'url?limit=10000&mimetype=application/octet-stream'
+	},
+	{
+		test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+		loader: 'url?limit=10000&mimetype=image/svg+xml'
+	},
+	{
+		test: /\.gif/,
+		loader: 'url-loader?limit=10000&mimetype=image/gif'
+	},
+	{
+		test: /\.jpg/,
+		loader: 'url-loader?limit=10000&mimetype=image/jpg'
+	},
+	{
+		test: /\.png/,
+		loader: 'url-loader?limit=10000&mimetype=image/png'
+	}
+];
---
+++ webpack.production.config.js
@@ -0,0 +1,19 @@
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+
+module.exports = {
+	entry: [
+		'./lib/index.js'
+	],
+	output: {
+		path: path.join(__dirname, 'public', 'build'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.js']
+	},
+	module: {
+		loaders: loaders
+	}
+};
